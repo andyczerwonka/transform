6b5c9232aeca8b51b98c5179bbfedfd35b59e1da
Andy Czerwonka <andy.czerwonka@gmail.com>
2014-09-20 14:32:30 -0600

    the beginnings of a more robust version of the transformer

--- src/main/scala/model/Met.scala
+++ src/main/scala/model/Met.scala
@@ -11,26 +11,23 @@ object Met {
   case object Daily extends Periodicity
   case object Monthly extends Periodicity
   case object Yearly extends Periodicity
-  val periodicities = Seq(Daily, Monthly, Yearly)
 
-  def transform(me: MeasureExport, ts: Int): Seq[Seq[String]] = {
+  def transform(me: MeasureExport): Seq[Seq[String]] = {
 
-    def perdiodicity(from: LocalDate)(to: LocalDate) = Days.daysBetween(from, to).getDays() match {
+    def perdiodicity(from: LocalDate, to: LocalDate) = Days.daysBetween(from, to).getDays() match {
       case 1 => Daily
       case x if x > 1 && x < 31 => Monthly
       case _ => Yearly
     }
 
-    val firstTwoDates = {
+    val from, to = {
       val ymd = DateTimeFormat.forPattern("yyyy-MM-dd")
       val dateIndex = me.head.indexWhere(_ == "Date")
-      me.tail.take(2).map(_(dateIndex)) match {
-        case Seq(d1, d2) => (ymd.parseLocalDate(d1), ymd.parseLocalDate(d2))
-      }
+      val dates = me.tail.take(2).map(_(dateIndex)).map(ymd.parseLocalDate(_)) 
+      dates(0)
+      dates(1)
     }
-
-    val p = perdiodicity(firstTwoDates._1)(_: LocalDate)
-    val sot = p(firstTwoDates._2)
+     val p = perdiodicity(from, to)
 
     ???
   }
